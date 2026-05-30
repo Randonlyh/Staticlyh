80252d0b899e285f4a2abf26c07d2d93d0c578c4
Randonlyh <45102159+Randonlyh@users.noreply.github.com>
2026-05-30 18:37:07 +0100

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,68 @@
+# Staticlyh
+
+Extremely basic static site generation for posts, converting Markdown to HTML. Created since I thought it would be a simple enough exercise.
+
+## How it works
+
+Supply some markdown files and a html page template, and then the script converts your file into valid html and places it into a copy of the template, ensuring every page looks the same across your blog.
+
+## How to use
+
+1. In your website folder, create a `_posts` folder containing all your `.md` files. (You can have subfolders 1 level deep in here)
+    * Markdown files should be titled in the format `YYYY-MM-DD-title-goes-here.md`
+    * You can skip the date but this reduces the variables you can use later (see `--dateof`)
+    * Any spaces will be converted to `-` for the final built files
+2. Create the same folder structure but with `posts` instead of `_posts` (Script currently doesn't create the output directory on its own)
+3. Create a `posts.htemplate` file containing the layout of your page
+    You can use the following variables inside this template file:
+
+    | Variable | Usage                                                                 |
+    | -------- | --------------------------------------------------------------------- |
+    | --title  | Uses the name of the Markdown file                                    |
+    | --text   | The contents of the Markdown file are placed here (Converted to HTML) |
+    | --dateof | Adds the date of the Markdown file from the filename (if it exists)   |
+    | --year   | Uses the current year                                                 |
+
+4. For each subfolder inside your `_posts` folder, you may create a template page that links to all pages within it named after the folder (e.g `./_posts/blogs` becomes `./blogs.html` on the root using `blogs.htemplate`).
+5. To create each entry link on such a page, you need to tell Staticlyh what part of the html page to duplicate and fill in. Below is an example for a `blogs.htemplate` like above:
+
+    ```html
+        <main>
+            --/
+            <section>
+                <a href="--path"><h2>--title</h2></a>
+                <time>Posted on: --dateof</time>
+                <p>--description</p>
+                <hr>
+            </section>
+            /--
+        </main>
+    ```
+
+    Notice the use of `--/ /--` around the part that needs to be duplicated, and the variable names. Staticlyh will paste everything between `--/ /--` right underneath until every page has been referenced.
+
+    You can use the following variables inside this template file:
+
+    | Variable       | Usage                                                                                                                |
+    | -------------- | -------------------------------------------------------------------------------------------------------------------- |
+    | --title        | Uses the name of the referenced Markdown file                                                                        |
+    | --path         | Puts in the final file path of the referenced page                                                                   |
+    | --description  | The first 200 characters of the referenced Markdown file are placed here, converted to HTML and removing any headers |
+    | --dateof       | Adds the date of the referenced Markdown file from the filename (if it exists)                                       |
+    | --year         | Uses the current year                                                                                                |
+
+6. Run the script with `./staticlyh.py build /path/to/your/site/`
+
+## Dependencies
+
+* Python 3 (Any recent version will probably work)
+* Python-Markdown (`pip install markdown`)
+
+## Notes
+
+* Windows style paths were not tested as I use Linux.
+* This is probably not the best tool for the job if you want a proper website of your own, as this requires a very specific website setup and many parts of your website still need to be copy-pasted around. Consider trying out something like [Jekyll](https://jekyllrb.com/) or similar.
+
+## AI Disclaimer
+
+None used!
---
+++ staticlyh.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python3
+import sys
+import datetime
+from pathlib import Path
+import os
+import re
+
+import markdown
+
+def help_message():
+	print("Arguments:")
+	print("--help [-h] - Prints this message")
+	print("build [b] \"path/to/source\" - Compiles the pages in _post and creates link pages for each subfolder inside it")
+	sys.exit()
+
+def build():
+	path = sys.argv[2]
+
+	# Loads the template html
+	template = path + "/posts.htemplate"
+	with open(template, 'r') as f:
+		template = f.read()
+	
+	# Grabs all the post paths
+	posts_path = path + "/_posts"
+	posts_path = Path(posts_path)
+	md_files = gather_files(posts_path)
+
+	# Find all folders
+	folders = []
+	for entry in posts_path.iterdir():
+		if entry.is_dir():
+			if "/_drafts" not in str(entry):
+				entry_split = str(entry).split("/")
+				folders.append(entry_split[-1])
+
+	# Calculate destination paths and store date if it exists in the original filename
+	html_files = md_files.copy()
+	date_of = list()
+	date_exists = list()
+	title = list()
+	for i in range(len(html_files)):
+		html_files[i] = html_files[i].replace("_", "")
+		html_files[i] = html_files[i].replace(" ", "-")
+		html_files[i] = html_files[i].replace(".md", ".html")
+		
+		date_exists.append(find_date(html_files[i]))
+
+		if date_exists[i] == True:
+			date_of.append(html_files[i].split("/")[-1][:10])
+		else:
+			date_of.append("Unknown")
+		
+	for i in range(len(md_files)):
+		with open(md_files[i], 'r', encoding="utf-8") as input:
+			text = input.read()
+		
+		print("Creating: " + html_files[i])
+		# Parsing out the title
+		path_split = md_files[i].split("/")
+		title.append(path_split[-1])
+
+		if date_exists[i] == True:
+			title[i] = title[i][11:]
+		
+		title[i] = title[i].replace(".md", "")
+		title[i] = title[i].replace("-", " ")
+
+		# Overwriting the template with data
+		out = template
+		out = out.replace("--text", markdown.markdown(text))
+		out = out.replace("--title", title[i])
+		out = out.replace("--year", datetime.datetime.now().strftime("%Y"))
+		out = out.replace("--dateof", (date_of[i]))
+
+		with open(html_files[i], 'w', encoding="utf-8") as output:
+			output.write(out)
+	
+	for entry in folders:
+		create_link_page(entry, path, md_files, html_files, title, date_of)
+	
+	print("All done!")
+	return
+
+def gather_files(path='.'):
+	folders = []
+	for entry in path.iterdir():
+		if entry.is_dir():
+			if "/_drafts" not in str(entry):
+				folders.append(entry)
+
+	list_of_files = []
+	for folder in folders:
+		for file in folder.iterdir():
+			if str(file).endswith(".md"):
+				list_of_files.append(str(file))
+	
+	if not list_of_files:
+		print("Error, no files to work on found. Aborting...")
+		sys.exit()
+	
+	return list_of_files
+
+def find_date(filename):
+	x = re.search("\\d{2,4}-\\d{1,2}\\-\\d{1,2}", filename)
+	if not x:
+		return False
+	
+	return True
+
+def create_link_page(name, path, md_files, html_files, title, date):
+	out = []
+	path_obj = Path(path + name + ".htemplate")
+	if not path_obj.exists():
+		print("Skipping: " + name + ".html (corresponding .htemplate does not exist)")
+		return
+
+	print("Creating: " + name + ".html (links page)")
+	with open(path + name + ".htemplate", 'r', encoding="utf-8") as page:
+		out = page.read()
+
+	out = out.replace("--year", datetime.datetime.now().strftime("%Y"))
+
+	# Keeping the part of the file that may need copy pasting
+	repeatable = out
+	repeatable = repeatable[repeatable.index("--/"):repeatable.index("/--")]
+	out = out.replace(repeatable, "")
+	repeatable = repeatable[3:]
+	repeatable += "/--"
+
+	for i in range(len(md_files)):
+		post_split = html_files[i].split("/")
+		
+		if post_split[-2] != name:
+			continue
+		
+		print("Adding: " +  html_files[i] + " to " + name + ".html")
+		text = ""
+
+		with open(md_files[i], 'r', encoding='utf-8') as page:
+			text = page.read()
+		
+		# Preview of the post
+		text = text[:200]
+		text = text.replace("#", "")
+		text += "..."
+		text = markdown.markdown(text)
+
+		# Getting the page link
+		link_split = html_files[i].split("/")
+		link = link_split[-3] + "/" + link_split[-2] + "/" + link_split[-1]
+
+		# Pasting in other page info
+		out = out.replace("/--", repeatable)
+		out = out.replace("--description", text)
+		out = out.replace("--dateof", date[i])
+		out = out.replace("--title", title[i])
+		out = out.replace("--path", link)
+
+	out = out.replace("--/", "")
+	out = out.replace("/--", "")
+
+	with open(path + name + ".html", 'w', encoding="utf-8") as page:
+		page.write(out)
+	
+	return
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		help_message()
+
+	elif sys.argv[1] == '--help' or sys.argv[1] == '-h':
+		help_message()
+
+	elif sys.argv[1] == 'build' or sys.argv[1] == 'b':
+		if len(sys.argv) < 3:
+			print("Error, path not supplied. Aborting...")
+			sys.exit()
+		
+		build()
